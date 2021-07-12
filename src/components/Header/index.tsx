c03a4f53f4182d5c77fe7f0a2ef0139aea485446
Paulo Steinberg <paulo@physiotec.org>
2021-07-11 22:13:14 -0400

    updating project. Stripe is working now

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logoImg from '/public/images/logo.svg';
+import { SignInButton } from '../SignInButton';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logoImg} alt="" />
+        <nav>
+          <a href="" className={styles.active}>
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
